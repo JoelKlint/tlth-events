827c125fafca61e72a6ff2fd19dd5daee102bb5a
Joel Klint <joel.klint@gmail.com>
2016-05-15 23:38:58 +0200

    Setting initialstate serverside

--- server/routes/app.js
+++ server/routes/app.js
@@ -6,12 +6,19 @@ import TempComp from '../../views/TempComp.js';
 import React from 'react';
 import { renderToString } from 'react-dom/server'
 import configureStore from '../../store/configureStore.jsx';
+import { Map, Set, OrderedSet } from 'immutable';
 
 router.get('/', cas.bounce, function(req, res) {
 	const username = req.session.cas_user;
-	const store = configureStore();
+
+	const initialState = {};
+	const store = configureStore(initialState);
+
 	const html = renderToString(<TempComp store={store}/>);
-	res.render('index', { html: html});
+
+	const state = JSON.stringify(store.getState());
+
+	res.render('index', { html: html, initialState: state });
 });
 
 export default router;
--- views/index.jsx
+++ views/index.jsx
@@ -11,10 +11,21 @@ import configureStore from '../store/configureStore.jsx';
 import VisibleApp from './components/VisibleApp.jsx';
 
 import TempComp from './tempComp.js';
+import Immutable, { OrderedSet, Map, Set } from 'immutable';
 
 injectTapEventPlugin();
 
-const store = configureStore();
+// Get initial state from server
+const stateFromServer = window.__INITIAL_STATE__;
+// Transform state to immutable
+const serverEvents = Immutable.fromJS(stateFromServer.events.serverSide).toSet();
+const localEvents = Immutable.fromJS(stateFromServer.events.local).toSet();
+const events = Map({ serverSide: serverEvents, local: localEvents });
+const guilds = Immutable.fromJS(stateFromServer.guilds).toOrderedSet();
+const activeGuilds = Immutable.fromJS(stateFromServer.activeGuilds).toSet();
+const initialState = { events, guilds, activeGuilds };
+
+const store = configureStore(initialState);
 
 ReactDOM.render(
 	<TempComp store={store}/>,
