59b3d0d3476b0673d327d6382169ed57df0e0d6e
Joel Klint <joel.klint@gmail.com>
2016-05-16 00:45:59 +0200

    Only reading initial state if sent from server

--- views/index.jsx
+++ views/index.jsx
@@ -15,16 +15,23 @@ import Immutable, { OrderedSet, Map, Set } from 'immutable';
 
 injectTapEventPlugin();
 
+const populateState = (stateFromServer) => {
+	const events = Map({
+		serverSide: Immutable.fromJS(stateFromServer.events.serverSide).toSet(),
+		local: Immutable.fromJS(stateFromServer.events.local).toSet() });
+	const guilds = Immutable.fromJS(stateFromServer.guilds).toOrderedSet();
+	const activeGuilds = Immutable.fromJS(stateFromServer.activeGuilds).toSet();
+	const user = Immutable.fromJS(stateFromServer.user);
+	return { events, guilds, activeGuilds, user };
+};
+
 // Get initial state from server
 const stateFromServer = window.__INITIAL_STATE__;
 // Transform state to immutable
-const events = Map({
-	serverSide: Immutable.fromJS(stateFromServer.events.serverSide).toSet(),
-	local: Immutable.fromJS(stateFromServer.events.local).toSet() });
-const guilds = Immutable.fromJS(stateFromServer.guilds).toOrderedSet();
-const activeGuilds = Immutable.fromJS(stateFromServer.activeGuilds).toSet();
-const user = Immutable.fromJS(stateFromServer.user);
-const initialState = { events, guilds, activeGuilds, user };
+let initialState = {};
+if(stateFromServer) {
+	initialState = populateState(stateFromServer);
+}
 
 const store = configureStore(initialState);
 
