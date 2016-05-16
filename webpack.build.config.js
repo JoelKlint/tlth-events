b4cd2f5b670ff46f421b08fcefedead33d6e93f3
Joel Klint <joel.klint@gmail.com>
2016-05-16 22:30:49 +0200

    fixed webpack config files

---
+++ webpack.build.config.js
@@ -0,0 +1,69 @@
+var webpack = require('webpack');
+var path = require('path');
+var merge = require('webpack-merge')
+
+var client = {
+	entry: [
+		'./views/index.jsx'
+	],
+	output: {
+		path: path.join(__dirname, 'public'),
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.(jsx|js)$/,
+				loaders: ['babel'],
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				loaders: ['style', 'css']
+			},
+		]
+	}
+}
+
+var server = {
+	target: 'node',
+	entry: [
+		'./server/serverRenderer.jsx'
+	],
+	output: {
+		path: path.join(__dirname, 'public'),
+		filename: 'serverRenderer.js',
+		libraryTarget: 'commonjs2'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.(jsx|js)$/,
+				loader: 'babel',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.json$/,
+				loader: 'json'
+			},
+			{
+				test: /\.css$/,
+				loader: 'css'
+			}
+		]
+	}
+}
+
+var common = {
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('production')
+		}),
+		new webpack.optimize.UglifyJsPlugin({
+			compress: { warnings: false }
+		}),
+		new webpack.optimize.OccurrenceOrderPlugin()
+	]
+}
+
+module.exports = [ merge(client, common), merge(server, common) ];
---
+++ webpack.dev.config.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+	entry: [
+		'webpack-dev-server/client?http://localhost:8080',
+		'webpack/hot/only-dev-server',
+		'./views/index.jsx'
+	],
+	output: {
+		path: 'views',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.(jsx|js)$/,
+				loaders: ['react-hot', 'babel'],
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				loaders: ['style', 'css']
+			},
+		]
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('development')
+		}),
+		new webpack.HotModuleReplacementPlugin()
+	],
+	devServer: {
+		hot: true,
+		inline: true,
+		contentBase: path.resolve(__dirname, 'views'),
+		proxy: {
+			'/api/*': 'http://localhost:3000'
+		}
+	}
+}
