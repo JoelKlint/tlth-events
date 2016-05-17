bf9932977ce855e94e2f51603152cb4899401354
Joel Klint <joel.klint@gmail.com>
2016-05-17 10:36:59 +0200

    Base does not require login. Login redirects to base

--- server/routes/app.js
+++ server/routes/app.js
@@ -4,12 +4,21 @@ import cas from '../middleware/cas';
 import User from '../../models/user';
 import renderFromServer from '../../public/serverRenderer';
 
-router.get('/', cas.bounce, function(req, res) {
-	const username = req.session.cas_user;
-	User.findOrCreate({ username: username }, function(err, user, wasCreated) {
-		const initialState = { user };
-		renderFromServer(res, initialState);
-	});
+router.get('/', function(req, res) {
+	if(req.session.cas_user) {
+		const username = req.session.cas_user;
+		User.findOrCreate({ username: username }, function(err, user, wasCreated) {
+			const initialState = { user };
+			renderFromServer(res, initialState);
+		});
+	}
+	else {
+		renderFromServer(res, {});
+	}
 });
+
+router.get('/login', cas.bounce, function(req, res) {
+	res.redirect('/');
+})
 
 export default router;
