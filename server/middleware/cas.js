577cb4ab0145aa4e5489fc31f1770de2a64eb782
Joel Klint <joel.klint@gmail.com>
2016-05-22 23:25:31 +0200

    Authentication is now bypassed during tests

--- server/middleware/cas.js
+++ server/middleware/cas.js
@@ -1,11 +1,12 @@
 import CASAuthentication from 'cas-authentication';
+const NODE_ENV = process.env.NODE_ENV;
 const cas = new CASAuthentication({
     cas_url     : 'https://cas.lu.se/cas',
     service_url : 'http://localhost:3000',
 		cas_version     : '2.0',
 		renew           : false,
-    is_dev_mode     : false,
-    dev_mode_user   : '',
+    is_dev_mode     : NODE_ENV === 'test' ? true : false,
+    dev_mode_user   : 'testuser',
     dev_mode_info   : {},
     session_name    : 'cas_user',
     session_info    : 'cas_userinfo',
