a12dddeffb84828b82f06ae85fb535c94989f7bb
Joel Klint <joel.klint@gmail.com>
2016-05-23 19:16:50 +0200

    Handle mongoose connection error

--- server/index.js
+++ server/index.js
@@ -4,7 +4,10 @@ import winston from 'winston';
 import mongoose from 'mongoose';
 const dbUri = 'mongodb://firefox:firefox@ds013260.mlab.com:13260/tlth-events-server';
 mongoose.connect(dbUri,	(err) => {
-	if(err) winston.warn('Could not connect to database: ' + dbUri);
+	if(err) {
+		winston.warn('Could not connect to database: ' + dbUri)
+		process.exit();
+	}
 });
 
 
