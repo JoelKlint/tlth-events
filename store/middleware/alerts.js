05006502299d3af96da86fc4807ae9bbd8c7c2cd
Joel Klint <joel.klint@gmail.com>
2016-08-16 21:12:51 +0200

    Added notifications for more happenings

--- store/middleware/alerts.js
+++ store/middleware/alerts.js
@@ -1,7 +1,11 @@
 import createActionSpy from '../actionSpy'
 import Alert from 'react-s-alert';
 
-import { ADD_NEW_EVENT_SUCCESS, ADD_NEW_EVENT_FAILURE } from '../../actions/EventActions'
+import {
+  ADD_NEW_EVENT_SUCCESS, ADD_NEW_EVENT_FAILURE,
+  DELETE_EVENT_SUCCESS, DELETE_EVENT_FAILURE,
+  EDIT_EVENT_SUCCESS, EDIT_EVENT_FAILURE
+} from '../../actions/EventActions'
 
 const agent = {
 	[ADD_NEW_EVENT_SUCCESS]: (action) => {
@@ -9,6 +13,18 @@ const agent = {
 	},
   [ADD_NEW_EVENT_FAILURE]: (action) => {
 		Alert.error('Event could not be saved')
+	},
+  [DELETE_EVENT_SUCCESS]: (action) => {
+		Alert.success('Event deleted')
+	},
+  [DELETE_EVENT_FAILURE]: (action) => {
+		Alert.error('Event could not be deleted')
+	},
+  [EDIT_EVENT_SUCCESS]: (action) => {
+		Alert.success('Event saved')
+	},
+  [EDIT_EVENT_FAILURE]: (action) => {
+		Alert.success('Event could not be saved')
 	}
 }
 
